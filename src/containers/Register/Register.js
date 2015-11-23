4cba0cad4860bd0420a8efc63167b94dc740567d
Zachary Erskine <bboplifa@gmail.com>
2015-11-23 21:01:19 +0200

    async validate spinners

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -3,13 +3,14 @@ import { AlertAutoDismissable } from 'components';
 import React, {Component, PropTypes} from 'react';
 import { reduxForm } from 'redux-form';
 import { validate as validateParams} from 'redux/modules/validate';
-
 import {connect} from 'react-redux';
-
+import {classnames as classNames} from 'classnames';
+var Loader = require('react-loader');
 
 export const fields = ['username', 'email', 'password'];
 
 let propToValidate;
+let validating = {};
 const validate = values => {
   const errors = {};
   if (!values.username) {
@@ -23,18 +24,20 @@ const validate = values => {
 
 const asyncValidate = (values, dispatch, _props) => {
   const propToValidate = _props.form._active;
-  return new Promise((resolve,reject) => {
+  if (values[propToValidate]) validating[[propToValidate]] = true
+  return new Promise((resolve, reject) => {
     dispatch(validateParams(values))
       .then(response => {
+        validating[[propToValidate]] = false
         if (response.error) return reject(response.error);
 
         return resolve();
       })
-      .catch(err => reject(err) )
+      .catch(err => reject(err))
   })
 }
 
-@connect( state => ({validate: state.validate}))
+@connect(state => ({validate: state.validate}))
 export default class Register extends Component {
   handleSubmit(event) {
     event.preventDefault();
@@ -45,6 +48,7 @@ export default class Register extends Component {
     //email.value = '';
     //password.value = '';
   }
+
   static propTypes = {
     fields: PropTypes.object.isRequired,
     resetForm: PropTypes.func.isRequired,
@@ -55,40 +59,46 @@ export default class Register extends Component {
 
   render() {
     const {validate, fields: {username, email, password}, resetForm, handleSubmit} = this.props;
-
     const styles = require('./Register.scss');
+    var options = {
+      speed: 1,
+      trail: 60,
+      shadow: false,
+      hwaccel: true,
+      left: '842px',
+      scale: 0.5,
+      position: 'relative'
+    };
+    let usernameSpinnerOptions = Object.assign({top:'18px'}, options);
+    let emailSpinnerOptions = Object.assign({top:'67px'}, options);
     return (
-      <form  className="form col-md-4" onSubmit={handleSubmit}>
-        <div className={'form-group' + (username.touched && username.error ? ' has-error' : '')}>
-          <input type="text" className="username form-control" placeholder="Username" {...username}/>
-          {validate.validatingUsername && <i className="fa fa-cog fa-spin" />}
-        </div>
-        {username.touched && username.error && <div className="help-block">{username.error}</div>}
-        <div className={'form-group' + (email.touched && email.error ? ' has-error' : '')}>
-          <input type="text" className="form-control" placeholder="Email" {...email}/>
-          {validate.validatingEemail && <i className="fa fa-cog fa-spin" /* spinning cog */ style={{
-              position: 'absolute',
-              right: 25,
-              top: 10
-            }}/>}
-        </div>
-        {email.touched && email.error && <div className="help-block">{email.error}</div>}
-        <div className={'form-group' + (password.touched && password.error ? ' has-error' : '')}>
-          <input type="text" className="form-control" placeholder="Password" {...password}/>
-          {false && <i className="fa fa-cog fa-spin" /* spinning cog */ style={{
-              position: 'absolute',
-              right: 25,
-              top: 10
-            }}/>}
-        </div>
-        {password.touched && password.error && <div className="help-block">{password.error}</div>}
+      <div className={styles.registerPage}>
+        <h1><span className="fa fa-user-plus"></span> Sign up</h1>
+        <div className="col-sm-6 col-sm-offset-3">
+          <form className="login-form" onSubmit={handleSubmit}>
+            <div className={'form-group' + (username.touched && username.error ? ' has-error' : '')}>
+              <input type="text" className="username form-control" placeholder="Username" {...username}/>
+                {validating.username && <Loader options={usernameSpinnerOptions} />}
+            </div>
+            {username.touched && username.error && <div className="help-block">{username.error}</div>}
+            <div className={'form-group' + (email.touched && email.error ? ' has-error' : '')}>
+              <input type="text" className="form-control" placeholder="Email" {...email}/>
+              {validating.email && <Loader options={emailSpinnerOptions} />}
+            </div>
+            {email.touched && email.error && <div className="help-block">{email.error}</div>}
+            <div className={'form-group' + (password.touched && password.error ? ' has-error' : '')}>
+              <input type="text" className="form-control" placeholder="Password" {...password}/>
+            </div>
+            {password.touched && password.error && <div className="help-block">{password.error}</div>}
 
 
-        <div className="text-center">
-          <button className="btn btn-primary btn-lg" style={{margin: 10}} onClick={handleSubmit}>Sign Up</button>
-          <button className="btn btn-default btn-lg" style={{margin: 10}} onClick={resetForm}>Clear Values</button>
+            <div className="text-center">
+              <button className="btn btn-primary btn-lg" style={{margin: 10}} onClick={handleSubmit}>Sign Up</button>
+              <button className="btn btn-default btn-lg" style={{margin: 10}} onClick={resetForm}>Clear Values</button>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     );
   }
 }
